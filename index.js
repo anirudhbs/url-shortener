8dcb20e80e30ff63eff9d52b2b98f7bdb5281eea
Anirudh B S <anirudhbs96@gmail.com>
2017-11-27 12:35:53 +0530

    Log url sent through post request

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const console = require('console')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) =>
   res.send('Hello world'))
 
-app.post('/shorten', (req, res) =>
-  res.send('shorten url'))
+app.post('/shorten', (req, res) => {
+  const inputURL = req.body.url
+  console.log(`url is ${inputURL}`)
+  res.send('shorten url')
+})
 
 app.get('/:hash', (req, res) =>
   res.send('get complete url'))
 
-app.listen(3000, () => console.log('listening on port 3000'))
+app.listen(3000, () =>
+  console.log('listening on port 3000'))
