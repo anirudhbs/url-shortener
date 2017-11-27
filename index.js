b654c97c88e2d60dd952035f6367d7a89aba2e4c
Anirudh B S <anirudhbs96@gmail.com>
2017-11-27 12:06:57 +0530

    Add express to dependencies, set up 2 routes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello')
+const console = require('console')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) =>
+  res.send('Hello world'))
+
+app.post('/shorten', (req, res) =>
+  res.send('shorten url'))
+
+app.get('/:hash', (req, res) =>
+  res.send('get complete url'))
+
+app.listen(3000, () => console.log('listening on port 3000'))
